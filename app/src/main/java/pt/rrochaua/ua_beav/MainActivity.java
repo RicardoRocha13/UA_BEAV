9bcc6fde9d0823767ce7994809c3b1ed3108850b
Filipe Marques <filipedrmarques@gmail.com>
2017-05-27 15:42:56 +0100

    start working on gallery

--- app/src/main/java/pt/rrochaua/ua_beav/MainActivity.java
+++ app/src/main/java/pt/rrochaua/ua_beav/MainActivity.java
@@ -411,6 +411,45 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
+            case PICK_IMAGE_MULTIPLE:
+                if(resultCode == RESULT_OK && null!= data){
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    imagesEncodedList = new ArrayList<String>();
+                    if(data.getData()!= null){
+                        Uri mImageUri = data.getData();
+
+                        //Get Cursor
+                        Cursor cursor = getContentResolver().query(mImageUri, filePathColumn, null, null, null);
+                        cursor.moveToFirst();
+
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        imageEncoded = cursor.getString(columnIndex);
+                        cursor.close();
+                    } else  {
+                        if (data.getClipData() != null){
+                            ClipData mClipData = data.getClipData();
+                            ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                            for(int i=0; i < mClipData.getItemCount(); i++){
+                                ClipData.Item item = mClipData.getItemAt(i);
+                                Uri uri = item.getUri();
+                                mArrayUri.add(uri);
+                                // Get the cursor
+                                Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+                                // Move to first row
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                imageEncoded  = cursor.getString(columnIndex);
+                                imagesEncodedList.add(imageEncoded);
+                                cursor.close();
+                            }
+                            Log.v("LOG_TAG", "select Images" + mArrayUri.size());
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+                }
+                break;
             case REQUEST_TAKE_PHOTO:
                 if (resultCode == Activity.RESULT_OK) {
                     galleryAddPic();
@@ -419,66 +458,8 @@ public class MainActivity extends AppCompatActivity
                     Toast.makeText(this, "Erro ao guardar a fotografia", Toast.LENGTH_LONG).show();
                 }
                 break;
-
         }
-
-        //TRATAR DESTE RESULT
-
-               /* try {
-                    // When an Image is picked
-                    if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
-                            && null != data) {
-                        // Get the Image from data
-
-                        String[] filePathColumn = { MediaStore.Images.Media.DATA };
-                        imagesEncodedList = new ArrayList<String>();
-                        if(data.getData()!=null){
-
-                            Uri mImageUri=data.getData();
-
-                            // Get the cursor
-                            Cursor cursor = getContentResolver().query(mImageUri,
-                                    filePathColumn, null, null, null);
-                            // Move to first row
-                            cursor.moveToFirst();
-
-                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                            imageEncoded  = cursor.getString(columnIndex);
-                            cursor.close();
-
-                        }else {
-                            if (data.getClipData() != null) {
-                                ClipData mClipData = data.getClipData();
-                                ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
-                                for (int i = 0; i < mClipData.getItemCount(); i++) {
-
-                                    ClipData.Item item = mClipData.getItemAt(i);
-                                    Uri uri = item.getUri();
-                                    mArrayUri.add(uri);
-                                    // Get the cursor
-                                    Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
-                                    // Move to first row
-                                    cursor.moveToFirst();
-
-                                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                                    imageEncoded  = cursor.getString(columnIndex);
-                                    imagesEncodedList.add(imageEncoded);
-                                    cursor.close();
-
-                                }
-                                Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
-                            }
-                        }
-                    } else {
-                        Toast.makeText(this, "You haven't picked Image",
-                                Toast.LENGTH_LONG).show();
-                    }
-                } catch (Exception e) {
-                    Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
-                            .show();
-                }
-
-                super.onActivityResult(requestCode, resultCode, data);*/
+        
     }
 
     //Secção Fragments
