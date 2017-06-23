012c5a393b1daf1cf30c31545432ad235ebb52ad
Ricardo Rocha <rrocha@ua.pt>
2017-06-23 16:02:36 +0100

    22/6 Finalização da formatação do texto do layout

--- app/src/main/java/pt/rrochaua/ua_beav/fragments/ConsPeo.java
+++ app/src/main/java/pt/rrochaua/ua_beav/fragments/ConsPeo.java
@@ -1,5 +1,6 @@
 package pt.rrochaua.ua_beav.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import pt.rrochaua.ua_beav.MainActivity;
 import pt.rrochaua.ua_beav.R;
@@ -62,6 +66,50 @@ public class ConsPeo extends Fragment {
                 parentActivity.goToConsPassFragment();
             }
         });
+
+
+        ImageButton btnChangeDate = (ImageButton) v.findViewById(R.id.btnDate);
+        final EditText eTextDia = (EditText) v.findViewById(R.id.eTDia);
+        final EditText eTextMes = (EditText) v.findViewById(R.id.eTMes);
+        final EditText eTextAno = (EditText) v.findViewById(R.id.eTAno);
+        final EditText eTHora = (EditText) v.findViewById(R.id.eTHora);
+        final EditText eTMin = (EditText) v.findViewById(R.id.eTMin);
+
+
+        btnChangeDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code to open fragment
+                final Dialog dateDialog = new Dialog(v.getContext());
+                dateDialog.setContentView(R.layout.date_picker);
+                dateDialog.show();
+
+                Button btnDateCancel = (Button) dateDialog.findViewById(R.id.btnDateCancel);
+                Button btnDateOk = (Button) dateDialog.findViewById(R.id.btnDateOk);
+                final DatePicker dP = (DatePicker) dateDialog.findViewById(R.id.datePicker);
+
+                btnDateCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dateDialog.dismiss();
+                    }
+                });
+
+                btnDateOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dateDialog.dismiss();
+                        eTextDia.setText(Integer.toString(dP.getDayOfMonth()));
+                        eTextMes.setText(Integer.toString(dP.getMonth() + 1));
+                        eTextAno.setText(Integer.toString(dP.getYear()));
+
+                    }
+                });
+
+            }
+        });
+
+
         return v;
     }
 
