4eb311102b251b55cf85e05f28db52d22f449f4e
Ricardo Rocha <rrocha@ua.pt>
2017-08-02 13:44:04 +0100

    2/08 modificação geral do edittext para nao passar para o seguinte ao premir enter, e modificação dos campos de data para nao permitir a escrita direta Ja guarda no CircExt2

--- app/src/main/java/pt/rrochaua/ua_beav/fragments/Menu.java
+++ app/src/main/java/pt/rrochaua/ua_beav/fragments/Menu.java
@@ -35,7 +35,6 @@ public class Menu extends Fragment {
 
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_menu, container, false);
