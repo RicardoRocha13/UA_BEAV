9460efb5efd2da440dc8e0895414937b5c0dc4d1
Ricardo Rocha <rrocha@ua.pt>
2017-08-06 16:49:04 +0100

    condsemvit

--- app/src/main/java/pt/rrochaua/ua_beav/fragments/CondIntSem.java
+++ app/src/main/java/pt/rrochaua/ua_beav/fragments/CondIntSem.java
@@ -1,10 +1,10 @@
 package pt.rrochaua.ua_beav.fragments;
 
 import android.app.Dialog;
+import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +12,24 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 import pt.rrochaua.ua_beav.MainActivity;
 import pt.rrochaua.ua_beav.R;
+import pt.rrochaua.ua_beav.models.SemVitim;
+import pt.rrochaua.ua_beav.models.SemVitimCondutor;
 
 
 public class CondIntSem extends Fragment {
     MainActivity parentActivity;
-
+    ArrayList<SemVitim> semvitim;
     private OnCondIntSemListener mListener;
 
 
@@ -38,7 +48,7 @@ public class CondIntSem extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         parentActivity = (MainActivity) this.getActivity();
-
+        semvitim = parentActivity.getsViti();
         super.onCreate(savedInstanceState);
 
     }
@@ -49,30 +59,47 @@ public class CondIntSem extends Fragment {
         // Inflate the layout for this fragment
         final View v = inflater.inflate(R.layout.fragment_cond_int_sem, container, false);
 
-        Button btnSegCondIntSem = (Button) v.findViewById(R.id.ButtonSegCondIntSem);
-        btnSegCondIntSem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parentActivity.goToFotoEsquemaFragment();
-            }
-        });
-
-
-        Button btnAntCondIntSem = (Button) v.findViewById(R.id.ButtonAntCondIntSem);
-        btnAntCondIntSem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parentActivity.goToForm1Fragment();
-            }
-        });
-
+        final RadioGroup rGroupVI = (RadioGroup) v.findViewById(R.id.radioGroupVI);
+        final RadioGroup rGroupCondP = (RadioGroup) v.findViewById(R.id.radioGroupCondP);
+        final RadioGroup rGroupSex = (RadioGroup) v.findViewById(R.id.radioGroupSex);
 
         ImageButton btnChangeDate = (ImageButton) v.findViewById(R.id.btnDate);
         final EditText eTextDia = (EditText) v.findViewById(R.id.eTDia);
         final EditText eTextMes = (EditText) v.findViewById(R.id.eTMes);
         final EditText eTextAno = (EditText) v.findViewById(R.id.eTAno);
-        final EditText eTHora = (EditText) v.findViewById(R.id.eTHora);
-        final EditText eTMin = (EditText) v.findViewById(R.id.eTMin);
+
+        Button btnSegCondIntSem = (Button) v.findViewById(R.id.ButtonSegCondIntSem);
+        Button btnAntCondIntSem = (Button) v.findViewById(R.id.ButtonAntCondIntSem);
+
+
+        final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        if(semvitim.size()>=1){
+
+
+
+            if (semvitim.get(0).condutorPresente==0){
+
+                /*
+                Date data = semvitim.get(0).idade;
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(data);
+                eTextDia.setText(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
+                int mes = cal.get(Calendar.MONTH)+1;
+                eTextMes.setText(String.valueOf(mes));
+                eTextAno.setText(String.valueOf(cal.get(Calendar.YEAR)));
+
+                ((RadioButton)rGroupSex.getChildAt(semvitim.get(0).genero)).setChecked(true);
+                */
+
+            }
+
+            ((RadioButton)rGroupCondP.getChildAt(semvitim.get(0).condutorPresente)).setChecked(true);
+            ((RadioButton)rGroupVI.getChildAt(semvitim.get(0).veiculo)).setChecked(true);
+
+
+        }
+
 
 
         btnChangeDate.setOnClickListener(new View.OnClickListener() {
@@ -107,6 +134,67 @@ public class CondIntSem extends Fragment {
 
             }
         });
+
+
+        btnSegCondIntSem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (rGroupCondP.getCheckedRadioButtonId() == -1 || rGroupVI.getCheckedRadioButtonId() == -1){
+                    Toast.makeText(parentActivity, "Todos os campos devem estar preenchidos.", Toast.LENGTH_LONG).show();
+                } else {
+
+                    Date idade = null;
+                    try {
+                        idade = sdf.parse(eTextDia.getText().toString() + "/" + eTextMes.getText().toString() + "/" + eTextAno.getText().toString() +
+                                " " );
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    int indexPresente = rGroupCondP.indexOfChild(rGroupCondP.findViewById(rGroupCondP.getCheckedRadioButtonId()));int Presente = rGroupCondP.indexOfChild(rGroupCondP.findViewById(rGroupCondP.getCheckedRadioButtonId()));
+                    int indexVeiculo = rGroupVI.indexOfChild(rGroupVI.findViewById(rGroupVI.getCheckedRadioButtonId()));
+
+
+                    if (rGroupVI.getCheckedRadioButtonId() == 0) {
+
+                        if (eTextDia.getText().toString().equals("") || rGroupSex.getCheckedRadioButtonId() == -1) {
+                            Toast.makeText(parentActivity, "Todos os campos devem estar preenchidos.", Toast.LENGTH_LONG).show();
+                        }
+                        int indexSex = rGroupSex.indexOfChild(rGroupSex.findViewById(rGroupSex.getCheckedRadioButtonId()));
+
+                        SemVitimCondutor SVC = new SemVitimCondutor(indexVeiculo, indexPresente, indexSex, idade);
+
+                        semvitim.add(0, SVC);
+                        parentActivity.setsViti(semvitim);
+                        parentActivity.goToFotoEsquemaFragment();
+
+                    }else{
+                        SemVitim SV = new SemVitim(indexVeiculo, indexPresente);
+                        semvitim.add(0, SV);
+                        parentActivity.setsViti(semvitim);
+                        parentActivity.goToFotoEsquemaFragment();
+                    }
+
+                }
+
+
+
+
+            }
+        });
+
+
+        btnAntCondIntSem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parentActivity.goToForm1Fragment();
+            }
+        });
+
+
+
+
 
         return v;
     }
