53b9f530b2fd598fcee0993a8abfd6c94e4c0841
Ricardo <rrocha@ua.pt>
2018-01-30 22:08:24 +0000

    mod para variação de veículo, passageiro e peão

---
+++ app/src/main/java/pt/rrochaua/ua_beav/models/Multiplo.java
@@ -0,0 +1,19 @@
+package pt.rrochaua.ua_beav.models;
+
+/**
+ * Created by Ric on 30/01/2018.
+ */
+
+public class Multiplo {
+
+
+    public int veiculo, passageiro;
+
+    public Multiplo(int veiculo, int passageiro){
+        this.veiculo = veiculo;
+        this.passageiro = passageiro;
+    }
+
+
+
+}
